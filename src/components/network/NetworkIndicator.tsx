6b78c1afc11bd0b167181df79733bb864e7b522e
ChrisNatixar <chrislewis00+natgit@gmail.com>
2024-05-13 22:39:45 +0200

    Update aligned dates in api request

--- src/components/network/NetworkIndicator.tsx
+++ src/components/network/NetworkIndicator.tsx
@@ -6,7 +6,7 @@ import { memo, useCallback, useEffect, useMemo, useState } from "react"
 import { useSelector } from "react-redux"
 import { TimeMeasurement } from "data/domain/types/time/TimeRelatedTypes"
 import { selectEmissionRangeRequestParameters } from "data/store/api/EmissionSelectors"
-import { formatISO, startOfMinute, startOfHour, startOfDay, startOfWeek, startOfMonth, startOfQuarter, startOfYear }  from "date-fns"
+import { DateTime } from "luxon";
 import {
   EmissionRangesRequest,
   formatProtocolForRangesEndpoint,
@@ -26,52 +26,75 @@ const NetworkIndicator = (props: SxProps) => {
   const { timeRangeOfInterest, protocol } = useSelector(
     selectEmissionRangeRequestParameters,
   )
-  const scale: string = `1${TimeMeasurement.MINUTES}`
+  const scale: string = `1${TimeMeasurement.DAYS}`
   const requestParams: EmissionRangesRequest = useMemo(
-    () =>
-      ({
+    () => {
+      const {alignedStart,alignedEnd} = getAlignedDates(scale, timeRangeOfInterest.start, timeRangeOfInterest.end);
+      return {
         protocol: formatProtocolForRangesEndpoint(protocol),
         scale,
         timeRanges: [
           {
-            start: formatISOByScale(timeRangeOfInterest.start, scale),
-            end: formatISOByScale(timeRangeOfInterest.end, scale),
+            start: alignedStart.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
+            end: alignedEnd.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
             scale,
           },
         ],
-      }) as EmissionRangesRequest,
-    [protocol, timeRangeOfInterest],
+      } as EmissionRangesRequest;
+    }, [protocol, timeRangeOfInterest],
   )
-  function formatISOByScale(timestamp: number, fullscale: string): string {
+
+  function getAlignedDates(fullscale : string, startTimestamp: number, endTimestamp: number) {
     let scale = fullscale[fullscale.length - 1];
-    let date = new Date(timestamp);  
-    switch (scale) {
-      case 'm':
-        date = startOfMinute(date);
+    let startDate = DateTime.fromMillis(startTimestamp);  
+    let endDate = DateTime.fromMillis(endTimestamp);
+    let result;  
+      switch (scale) {
+        case 'm':
+		    result = alignedDates(startDate, endDate, { second: 0 }, 'minute');
         break;
-      case 'h':
-        date = startOfHour(date);
+        case 'h':
+        result = alignedDates(startDate, endDate, { minute: 0, second: 0 }, 'hour');
         break;
-      case 'd':
-        date = startOfDay(date);
+        case 'd':
+        result = alignedDates(startDate, endDate, { hour: 0, minute: 0, second: 0 }, 'day');
         break;
       case 'w':
-        date = startOfWeek(date);
+        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, localWeekDay:1}, 'week');
         break;
       case 'M':
-        date = startOfMonth(date);
+        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1}, 'month');
         break;
       case 'Q':
-        date = startOfQuarter(date);
+        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1}, 'quarter');
         break;
       case 'y':
-        date = startOfYear(date);
+        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1, month: 1}, 'year'); 
         break;
       default:
-        break;
+	     return  { alignedStart: startDate, alignedEnd: endDate };
+       break;
     }
-    return formatISO(date, { representation: 'complete' });
-  }
+    return { alignedStart: result.start, alignedEnd: result.end };
+}
+ 
+function alignedDates(startDate : DateTime, endDate: DateTime, resetUnits: any, incrementUnit: any) {
+    let start = startDate.set(resetUnits);
+    let end = endDate.set(resetUnits);
+
+	if (incrementUnit == 'quarter') {
+		// custom for quarters
+		start = start.set({month: Math.floor((start.month - 1) / 3) * 3 + 1});
+		end = end.set({month: Math.floor((end.month - 1) / 3) * 3 + 1});
+		if (startDate >= start) {
+         end = end.plus({ ['month']: 3 });
+		}
+	} else if (startDate >= start) {
+         end = end.plus({ [incrementUnit]: 1 });
+	}
+    return { start, end };
+}
+
   useGetEmissionRangesQuery(requestParams, {
     pollingInterval: 10000,
   })
