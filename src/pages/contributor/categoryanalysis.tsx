daa53cc9430db20c93f2d7e4ddbdf3a4287a5a57
Mathias <duble.mathias@gmail.com>
2024-05-21 15:14:07 +0200

    feat(navigation): add breadcrumb to cat analysis page

--- src/pages/contributor/categoryanalysis.tsx
+++ src/pages/contributor/categoryanalysis.tsx
@@ -1,10 +1,13 @@
 // material-ui
-import { Button, Grid, Typography } from "@mui/material"
+import { Button, Grid, Typography, Stack } from "@mui/material"
 import { CategoryCard } from "sections/contributor/category-analysis/CategoryCard"
 import { CategoryCalcTable } from "components/natixarComponents/CategoryCalcTable"
 import MainCard from "components/MainCard"
 import { useNavigate, useParams } from "react-router-dom"
-import { ArrowLeftOutlined } from "@ant-design/icons"
+import { ArrowLeftOutlined, RightOutlined } from "@ant-design/icons"
+import { selectAlignedIndexes } from "data/store/api/EmissionSelectors"
+import Breadcrumb from "../../components/@extended/Breadcrumbs"
+import { useSelector } from "react-redux"
 
 // table data
 const createData = (year: number, methodology: string, amount: number) => ({
@@ -18,30 +21,60 @@ const calculationMethods: any[] = [
   // createData(2024, "Emission Factors", 63.5),
 ]
 
+
+
 const CategoryAnalysis = () => {
   const { id: idStr } = useParams()
   const scopeId = parseInt(idStr!, 10)
   const navigate = useNavigate()
 
+  const alignedIndexes = useSelector(selectAlignedIndexes)
+  const categoryName = alignedIndexes.categories[scopeId]?.name
+
+  const links = [
+    {
+      title: "Dashboard",
+      to: "/",
+    },
+    {
+      title: `${categoryName ?? " Total "} top contributors`,
+      to: "",
+    },
+  ]
+
   return (
     <>
+      <Stack>
+        <Button
+          onClick={() => {
+            navigate(-1)
+          }}
+          sx={{
+            color: "primary.contrastText",
+            width: "fit-content",
+            marginBottom: "1.9rem",
+          }}
+          variant="contained"
+          startIcon={<ArrowLeftOutlined color="primary.contrastText" />}
+        >
+          Back
+        </Button>
+        <Breadcrumb
+          custom
+          title={false}
+          links={links}
+          separator={RightOutlined}
+          sx={{
+            mb: "0px !important",
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        />
+      </Stack>
       <Typography variant="h5" sx={{ marginBottom: "30px" }}>
         Category Analysis
       </Typography>
-      <Button
-        onClick={() => {
-          navigate(-1)
-        }}
-        sx={{
-          color: "primary.contrastText",
-          width: "fit-content",
-          marginBottom: "1.9rem",
-        }}
-        variant="contained"
-        startIcon={<ArrowLeftOutlined color="primary.contrastText" />}
-      >
-        Back
-      </Button>
       <Grid container rowSpacing={4.5} columnSpacing={3}>
         <Grid item xs={12} md={4}>
           <CategoryCard categoryId={scopeId} />
