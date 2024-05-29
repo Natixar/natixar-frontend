9862b3fbc862d76f703af3a27229d3738d805501
Math712 <duble.matias@gmail.com>
2024-05-29 15:39:19 +0200

    quick apply my setting

--- src/data/store/features/emissions/ranges/EmissionRangesClient.ts
+++ src/data/store/features/emissions/ranges/EmissionRangesClient.ts
@@ -1,6 +1,10 @@
 import { createApi } from "@reduxjs/toolkit/query/react"
 import { backendBaseQuery } from "data/store/config/BackendConfigs"
-import { EmissionRangesRequest, EmissionRangesPayload, EmissionResponse } from "./EndpointTypes"
+import {
+  EmissionRangesRequest,
+  EmissionRangesPayload,
+  EmissionResponse,
+} from "./EndpointTypes"
 
 const encodeRangeParameters = (r: EmissionRangesRequest): string => {
   const parameterString =
@@ -15,11 +19,11 @@ export const emissionRangesApi = createApi({
   baseQuery: backendBaseQuery(),
   endpoints: (builder) => ({
     getEmissionRanges: builder.query<
-       //EmissionRangesPayload,
+      // EmissionRangesPayload,
       EmissionResponse,
       EmissionRangesRequest
     >({
-      query: (request) => ({
+      query: (request: EmissionRangesRequest) => ({
         url: `/api/v0/data/ranges?${encodeRangeParameters(request)}`,
         method: "GET",
       }),
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import viteTsconfigPaths from "vite-tsconfig-paths"
+import { resolve } from "path"
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: process.env.VITE_APP_BASE_NAME,
   plugins: [react(), viteTsconfigPaths()],
   define: {
-    global: 'window'
+    global: "window",
   },
   resolve: {
     alias: [
@@ -24,6 +25,28 @@ export default defineConfig({
       //   find: 'assets',
       //   replacement: path.join(process.cwd(), 'src/assets')
       // },
+      { find: "assets", replacement: resolve(__dirname, "./src/assets") },
+      { find: "contexts", replacement: resolve(__dirname, "./src/contexts") },
+      { find: "routes", replacement: resolve(__dirname, "./src/routes") },
+      { find: "themes", replacement: resolve(__dirname, "./src/themes") },
+      {
+        find: "components",
+        replacement: resolve(__dirname, "./src/components"),
+      },
+      { find: "data", replacement: resolve(__dirname, "./src/data") },
+      { find: "utils", replacement: resolve(__dirname, "./src/utils") },
+      { find: "hooks", replacement: resolve(__dirname, "./src/hooks") },
+      { find: "hooks", replacement: resolve(__dirname, "./src/hooks") },
+      { find: "layout", replacement: resolve(__dirname, "./src/layout") },
+      { find: "pages", replacement: resolve(__dirname, "./src/pages") },
+      { find: "types", replacement: resolve(__dirname, "./src/types") },
+      { find: "api", replacement: resolve(__dirname, "./src/api") },
+      { find: "sections", replacement: resolve(__dirname, "./src/sections") },
+      {
+        find: "menu-items",
+        replacement: resolve(__dirname, "./src/menu-items"),
+      },
+      { find: "config", replacement: resolve(__dirname, "./src/config.ts") },
     ],
   },
   server: {
@@ -31,5 +54,5 @@ export default defineConfig({
     open: true,
     // this sets a default port to 3000
     port: 3000,
-  }
-});
+  },
+})
