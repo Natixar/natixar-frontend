ad88b270d9a4b2300a6e3e38cec96e9c87b3b6a9
Math712 <duble.matias@gmail.com>
2024-06-05 09:48:26 +0200

    refactor: lint

--- src/components/network/NetworkIndicator.tsx
+++ src/components/network/NetworkIndicator.tsx
@@ -1,8 +1,8 @@
-import { Box, Popover, SxProps, Tooltip, Typography } from "@mui/material"
+import { Box, SxProps, Tooltip } from "@mui/material"
 import SensorsIcon from "@mui/icons-material/Sensors"
 import SensorsOffIcon from "@mui/icons-material/SensorsOff"
 import { useGetNetworkInformationQuery } from "data/store/features/networkIndication/NetworkCheckClient"
-import { memo, useCallback, useEffect, useMemo, useState } from "react"
+import { memo, useEffect, useMemo } from "react"
 import { useSelector } from "react-redux"
 import { TimeMeasurement } from "data/domain/types/time/TimeRelatedTypes"
 import { selectEmissionRangeRequestParameters } from "data/store/api/EmissionSelectors"
@@ -23,77 +23,117 @@ const NetworkIndicator = (props: SxProps) => {
     pollingInterval: 2000,
   })
 
+  function alignedDates(
+    startDate: DateTime,
+    endDate: DateTime,
+    resetUnits: any,
+    incrementUnit: any,
+  ) {
+    let start = startDate.set(resetUnits)
+    let end = endDate.set(resetUnits)
+
+    if (incrementUnit === "quarter") {
+      // custom for quarters
+      start = start.set({ month: Math.floor((start.month - 1) / 3) * 3 + 1 })
+      end = end.set({ month: Math.floor((end.month - 1) / 3) * 3 + 1 })
+      if (startDate >= start) {
+        end = end.plus({ month: 3 })
+      }
+    } else if (startDate >= start) {
+      end = end.plus({ [incrementUnit]: 1 })
+    }
+    return { start, end }
+  }
+
+  function getAlignedDates(
+    fullscale: string,
+    startTimestamp: number,
+    endTimestamp: number,
+  ) {
+    const startDate = DateTime.fromMillis(startTimestamp)
+    const endDate = DateTime.fromMillis(endTimestamp)
+    let result
+    switch (fullscale[fullscale.length - 1]) {
+      case "m":
+        result = alignedDates(startDate, endDate, { second: 0 }, "minute")
+        break
+      case "h":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { minute: 0, second: 0 },
+          "hour",
+        )
+        break
+      case "d":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { hour: 0, minute: 0, second: 0 },
+          "day",
+        )
+        break
+      case "w":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { hour: 0, minute: 0, second: 0, localWeekDay: 1 },
+          "week",
+        )
+        break
+      case "M":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { hour: 0, minute: 0, second: 0, day: 1 },
+          "month",
+        )
+        break
+      case "Q":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { hour: 0, minute: 0, second: 0, day: 1 },
+          "quarter",
+        )
+        break
+      case "y":
+        result = alignedDates(
+          startDate,
+          endDate,
+          { hour: 0, minute: 0, second: 0, day: 1, month: 1 },
+          "year",
+        )
+        break
+      default:
+        result = { start: startDate, end: endDate }
+        break
+    }
+    return { alignedStart: result.start, alignedEnd: result.end }
+  }
+
   const { timeRangeOfInterest, protocol } = useSelector(
     selectEmissionRangeRequestParameters,
   )
   const scale: string = `1${TimeMeasurement.HOURS}`
-  const requestParams: EmissionRangesRequest = useMemo(
-    () => {
-      const {alignedStart,alignedEnd} = getAlignedDates(scale, timeRangeOfInterest.start, timeRangeOfInterest.end);
-      return {
-        protocol: formatProtocolForRangesEndpoint(protocol),
-        scale,
-        timeRanges: [
-          {
-            start: alignedStart.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
-            end: alignedEnd.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
-            scale,
-          },
-        ],
-      } as EmissionRangesRequest;
-    }, [protocol, timeRangeOfInterest],
-  )
-
-  function getAlignedDates(fullscale : string, startTimestamp: number, endTimestamp: number) {
-    let scale = fullscale[fullscale.length - 1];
-    let startDate = DateTime.fromMillis(startTimestamp);  
-    let endDate = DateTime.fromMillis(endTimestamp);
-    let result;  
-      switch (scale) {
-        case 'm':
-		    result = alignedDates(startDate, endDate, { second: 0 }, 'minute');
-        break;
-        case 'h':
-        result = alignedDates(startDate, endDate, { minute: 0, second: 0 }, 'hour');
-        break;
-        case 'd':
-        result = alignedDates(startDate, endDate, { hour: 0, minute: 0, second: 0 }, 'day');
-        break;
-      case 'w':
-        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, localWeekDay:1}, 'week');
-        break;
-      case 'M':
-        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1}, 'month');
-        break;
-      case 'Q':
-        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1}, 'quarter');
-        break;
-      case 'y':
-        result = alignedDates(startDate, endDate, {hour: 0, minute: 0, second: 0, day:1, month: 1}, 'year'); 
-        break;
-      default:
-	     return  { alignedStart: startDate, alignedEnd: endDate };
-       break;
-    }
-    return { alignedStart: result.start, alignedEnd: result.end };
-}
- 
-function alignedDates(startDate : DateTime, endDate: DateTime, resetUnits: any, incrementUnit: any) {
-    let start = startDate.set(resetUnits);
-    let end = endDate.set(resetUnits);
-
-	if (incrementUnit == 'quarter') {
-		// custom for quarters
-		start = start.set({month: Math.floor((start.month - 1) / 3) * 3 + 1});
-		end = end.set({month: Math.floor((end.month - 1) / 3) * 3 + 1});
-		if (startDate >= start) {
-         end = end.plus({ ['month']: 3 });
-		}
-	} else if (startDate >= start) {
-         end = end.plus({ [incrementUnit]: 1 });
-	}
-    return { start, end };
-}
+  const requestParams: EmissionRangesRequest = useMemo(() => {
+    const { alignedStart, alignedEnd } = getAlignedDates(
+      scale,
+      timeRangeOfInterest.start,
+      timeRangeOfInterest.end,
+    )
+    return {
+      protocol: formatProtocolForRangesEndpoint(protocol),
+      scale,
+      timeRanges: [
+        {
+          start: alignedStart.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
+          end: alignedEnd.toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
+          scale,
+        },
+      ],
+    } as EmissionRangesRequest
+  }, [protocol, timeRangeOfInterest])
 
   useGetEmissionRangesQuery(requestParams, {
     pollingInterval: 10000,
